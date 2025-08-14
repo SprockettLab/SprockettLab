d0009fc2c2c7b828837025768430dcc4f2a07a44
DanielSprockett <DanielSprockett@users.noreply.github.com>
2025-08-14 17:45:19 +0000

    Deploying to gh-pages from @ SprockettLab/SprockettLab@bcd359b3a8a47f775a5a657a5587006fc4a21005 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-the-sprockett-lab-is-officially-ready-for-action",
+          title: 'The Sprockett Lab is officially ready for action!',
+          description: "",
+          section: "News",},{id: "news-alyssa-joined-the-lab-welcome-alyssa-sparkles-smile",
+          title: 'Alyssa joined the lab. Welcome Alyssa! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-microbiome-assembly-in-early-life",
           title: 'Microbiome Assembly in Early Life',
