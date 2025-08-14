3e4dbb810208f0fc038d2a4c93cebaec371e0efe
DanielSprockett <DanielSprockett@users.noreply.github.com>
2025-08-14 17:28:44 +0000

    Deploying to gh-pages from @ SprockettLab/SprockettLab@524721378efae1fedd4f1ef34b6caf1cf76978d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-microbiome-assembly-in-early-life",
+          title: 'Microbiome Assembly in Early Life',
+          description: "Where do our microbiomes come from?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
